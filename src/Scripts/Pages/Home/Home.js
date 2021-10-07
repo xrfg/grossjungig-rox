5d82e4c2e7f4fa1495355b5a6001c8a4a9a89212
henriquemartins <henrm@riseup.net>
2021-10-07 12:00:24 +0200

    initializing home page

---
+++ src/Scripts/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+// * React imports
+import React from "react";
+
+// * Stylesheet import
+import "./home.scss";
+
+// * Component imports
+import Hero from "../../Components/Hero/Hero";
+
+export default function Home() {
+  return (
+    <div className="page">
+      <Hero />
+      im home
+    </div>
+  );
+}
